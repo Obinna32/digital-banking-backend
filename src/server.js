833a32069882f8114709acf0e01d7b1e4d51c14d
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-09-04 21:05:24 +0100

    feat: setup express server and mongodb connection

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.status(200).json({status: 'OK', message: "Digital Banking System API is live..."});
+});
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on PORT ${PORT}`);
+        
+    })
+})
